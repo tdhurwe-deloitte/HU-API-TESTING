f783fd42498504d18c69821f6eece1caa0d16aa7
tdhurwe-deloitte <tdhurwe@deloitte.com>
2022-03-31 12:23:06 +0530

    printing content-type

--- Rest_assured/src/test/java/RestAssuredTest.java
+++ Rest_assured/src/test/java/RestAssuredTest.java
@@ -39,6 +39,13 @@ public class RestAssuredTest {
     @Test
     public void testPutCall(){
         File jsonData = new File("src/main/resources/putData.json");
+        Response response = RestAssured
+                .given()
+                .get("https://jsonplaceholder.typicode.com/posts")
+                .then()
+                .extract()
+                .response();
+        List<Header> allValue = response.getHeaders().getList("Content-Type");
         given().
                 baseUri("https://reqres.in/api").
                 body(jsonData).
@@ -48,5 +55,11 @@ public class RestAssuredTest {
         then().
                 body("name",is(equalTo("Arun")), "job", is(equalTo("Manager"))).
                 statusCode(200);
+
+        for(Header header : allValue)
+        {
+            System.out.print(header.getName() +" : ");
+            System.out.println(header.getValue());
+        }
     }
 }
